737e4fd26e48bc9fe66fc812b05c910075f45938
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-13 14:28:54 +0100

    autowired PizzaRepository and added pizza list to model

--- src/main/java/org/lessons/java/spring_crud/controller/PizzeController.java
+++ src/main/java/org/lessons/java/spring_crud/controller/PizzeController.java
@@ -1,15 +1,26 @@
 package org.lessons.java.spring_crud.controller;
 
+import java.util.List;
+
+import org.lessons.java.spring_crud.model.Pizza;
+import org.lessons.java.spring_crud.repository.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class PizzeController {
+
+    @Autowired
+    private PizzaRepository repo;
     
     @GetMapping("/")
-    public String Homepage(){
+    public String Homepage(Model model){
+        List<Pizza> pizze = repo.findAll();
+        model.addAttribute("pizze", pizze);
         return "index";
     }
 }
