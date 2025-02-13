3109b48efad6399d63d9251f7eca2f48e837fb20
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-13 14:15:45 +0100

    added fragments and index html and created mapping for index file

--- src/main/java/org/lessons/java/spring_crud/controller/PizzeController.java
+++ src/main/java/org/lessons/java/spring_crud/controller/PizzeController.java
@@ -1,10 +1,15 @@
 package org.lessons.java.spring_crud.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class PizzeController {
     
+    @GetMapping("/")
+    public String Homepage(){
+        return "index";
+    }
 }
